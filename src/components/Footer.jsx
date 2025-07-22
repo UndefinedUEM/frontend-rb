164c115c52f1d485413e4c9beda0244338f1f0ec
Ingrid Lohmann <ingrid.lohmann@db1.com.br>
2025-07-22 03:03:32 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,17 +2,19 @@ import { Box, Text } from '@chakra-ui/react';
 import { useMemo } from 'react';
 
 const footerMessages = [
-  'Feito com café, código e carinho — equipe <undefined />',
-  'Feito por humanos (e alguns commits de madrugada) — equipe <undefined />',
-  'Feito na base do improviso e da paixão — equipe <undefined />',
-  'Feito por gente que testa em produção — equipe <undefined />',
-  'Feito com ❤️ e alguns bugs — equipe <undefined />',
-  'Feito por quem ama código… e sofre com ele — equipe <undefined />',
-  'Feito com <div>, emoção e push forçado — equipe <undefined />',
-  'Feito com bugs, mas com orgulho — equipe <undefined />',
-  'Feito com a coragem de quem não leu a doc — equipe <undefined />',
-  'Feito por devs que não sabem nomear variáveis — equipe <undefined />',
+  'Feito com café, código e carinho',
+  'Feito por humanos (e alguns commits de madrugada)',
+  'Feito na base do improviso e da paixão',
+  'Feito por gente que testa em produção',
+  'Feito com ❤️ e alguns bugs',
+  'Feito por quem ama código… e sofre com ele',
+  'Feito com <div>, emoção e push forçado',
+  'Feito com bugs, mas com orgulho',
+  'Feito com a coragem de quem não leu a doc',
+  'Feito por devs que não sabem nomear variáveis',
 ];
+
+const teamName = "equipe <undefined />";
 
 const Footer = () => {
   const randomMessage = useMemo(() => {
@@ -23,7 +25,10 @@ const Footer = () => {
   return (
     <Box as="footer" py={2} textAlign="center" color="gray.600" fontSize="sm">
       <Text fontSize='xs'>
-        {randomMessage}
+        {randomMessage} — {' '}
+        <Text as='b' color="gray.500">
+          {teamName}
+        </Text>
       </Text>
     </Box>
   );
